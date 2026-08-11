db42d293ac02d5752ade496d6faec6043eb3ad93
Aaron <tk11703@outlook.com>
2026-08-10 22:15:04 -0500

    adding local persistence on the postgres resource

--- src/TripPlanner.AppHost/AppHost.cs
+++ src/TripPlanner.AppHost/AppHost.cs
@@ -4,6 +4,7 @@ var postgresUser = builder.AddParameter("postgres-user", secret: false);
 var postgresPassword = builder.AddParameter("postgres-password", secret: true);
 
 var postgres = builder.AddPostgres("postgres", postgresUser, postgresPassword)
+    .WithDataVolume()
     .WithPgAdmin()
     .WithLifetime(ContainerLifetime.Persistent);
 
